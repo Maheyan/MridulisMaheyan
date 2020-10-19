bf00c1410f020deb3e676f2c5beb2e180caadfe9
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-10-19 19:12:20 +0600

    new

---
+++ Automation/js/firebase.js
@@ -0,0 +1,17 @@
+var app_firebase = {};
+(function () { 
+    var firebaseConfig = {
+        apiKey: "AIzaSyBs_QL_YWhueXi5ZhYLeS9sxtnM42RIovo",
+        authDomain: "automation-10-ms.firebaseapp.com",
+        databaseURL: "https://automation-10-ms.firebaseio.com",
+        projectId: "automation-10-ms",
+        storageBucket: "automation-10-ms.appspot.com",
+        messagingSenderId: "631976397067",
+        appId: "1:631976397067:web:d364ba280da1e29f29fa0e",
+        measurementId: "G-787N0P3H46"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  app_firebase = firebase;
+})()
