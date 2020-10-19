b8cfeea71af9aabb2995fce6d40b596b198875e5
Maheyan <68837961+Maheyan@users.noreply.github.com>
2020-10-20 01:08:35 +0600

    new

--- Automation/js/login.js
+++ Automation/js/login.js
@@ -1,33 +1,32 @@
 (function () {
-    // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
     var uiConfig = {
         callbacks: {
           signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
             return true;
           },
           uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
             document.getElementById('loader').style.display = 'none';
           }
         },
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
         signInSuccessUrl: 'index.html',
         signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.EmailAuthProvider.PROVIDER_ID
-
         ],
-        // Terms of service url.
-        tosUrl: 'term.html',
-        // Privacy policy url.
-        privacyPolicyUrl: 'term.html'
       }; 
-      // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+})()
+
+      var mainApp = {};
+(function() {
+    var firebase = app_firebase;
+var uid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    uid = user.uid;
+    window.location.replace("index.html");
+  }
+});
 })()
